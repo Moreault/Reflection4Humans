f748022585aaf81434917fd9d482b49951104eae
Mathieu Moreault <seranx@gmail.com>
2024-01-07 12:16:34 -0500

    Updated nugets and removed TypeGenerator. It will be part of another package in 3.0.0

--- Reflection4Humans.TypeGenerator.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using System.Net.NetworkInformation;
-global using AutoFixture;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using ToolBX.Eloquentest;
-global using FluentAssertions;
-global using ToolBX.Reflection4Humans.Extensions;
-global using ToolBX.Reflection4Humans.TypeGenerator;
-global using ToolBX.Reflection4Humans.TypeGenerator.Resources;
--- Reflection4Humans.TypeGenerator/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-// Global using directives
-
-global using System.Reflection;
-global using System.Reflection.Emit;
-global using ToolBX.Reflection4Humans.Extensions;
-global using ToolBX.Reflection4Humans.TypeGenerator.Resources;
