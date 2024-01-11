ba6b8ceb34bd26c09e97664f9ae28fab975b159b
Mathieu Moreault <seranx@gmail.com>
2024-01-10 19:50:07 -0500

    Removed needless reference to TImeProvider from unit tests and made a few last minute tweaks

--- Reflection4Humans.TypeFetcher.Tests/Dummies/Dummy.cs
+++ Reflection4Humans.TypeFetcher.Tests/Dummies/Dummy.cs
@@ -13,5 +13,5 @@ public record Dummy : IDummy
     public int Id { get; init; }
     public string Name { get; init; } = string.Empty;
     public string Description { get; init; } = string.Empty;
-    public DateTimeOffset DateCreated { get; init; } = ToolBX.TimeProvider.TimeProvider.Now;
+    public DateTimeOffset DateCreated { get; init; } = DateTimeOffset.UtcNow;
 }
--- Reflection4Humans.TypeFetcher.Tests/GlobalUsings.cs
+++ Reflection4Humans.TypeFetcher.Tests/GlobalUsings.cs
@@ -1,13 +1,12 @@
 // Global using directives
 
+global using FluentAssertions;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Reflection4Humans.TypeFetcher.Tests.Dummies;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Reflection;
-global using FluentAssertions;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Reflection4Humans.TypeFetcher.Tests.Dummies;
 global using ToolBX.Eloquentest;
 global using ToolBX.Reflection4Humans.Extensions;
 global using ToolBX.Reflection4Humans.TypeFetcher;
-global using ToolBX.TimeProvider;
